bdfd1f6e7358c130902f919b213eef40424e551d
FjellOverflow <fjelloverflow@protonmail.com>
2024-01-16 21:38:03 +0200

    (conf): adjust site url and base url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://fjelloverflow.github.io/blog/", // replace this with your deployed domain
+  website: "https://fjelloverflow.github.io/", // replace this with your deployed domain
   author: "Sat Naing",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
