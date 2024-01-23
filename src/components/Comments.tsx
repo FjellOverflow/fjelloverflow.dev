8293f39873610341ce39d3d8fb483cbbb48c5f84
FjellOverflow <fjelloverflow@protonmail.com>
2024-01-23 21:43:59 +0200

    feat(comments): add giscus for comments

---
+++ src/components/Comments.tsx
@@ -0,0 +1,38 @@
+import Giscus, { type GiscusProps, type Theme } from "@giscus/react";
+import { GISCUS } from "@config";
+import { useState } from "react";
+
+interface Props {
+  lightTheme?: Theme;
+  darkTheme?: Theme;
+}
+
+export default function Comments({
+  lightTheme = "light",
+  darkTheme = "dark",
+}: Props) {
+  const currentTheme = localStorage.getItem("theme");
+  const browserTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+
+  const [theme, setTheme] = useState(currentTheme || browserTheme);
+
+  // manual theme toggle
+  document.querySelector("#theme-btn")?.addEventListener("click", () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  });
+
+  // browser preference
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", ({ matches: isDark }) => {
+      isDark ? setTheme("dark") : setTheme("light");
+    });
+
+  return (
+    <div className="mt-8">
+      <Giscus theme={theme === "light" ? lightTheme : darkTheme} {...GISCUS} />
+    </div>
+  );
+}
