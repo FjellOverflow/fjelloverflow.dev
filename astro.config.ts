705e551bea0bc66fab5df081baf70f968a79ac14
FjellOverflow <fjelloverflow@protonmail.com>
2024-03-16 19:12:55 +0200

    feat: restyle

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      experimentalThemes: {
+        dark: "nord",
+        light: "github-light",
+      },
       wrap: true,
     },
   },
